e844753f86ce42301f4e54cf919ff83de11e27ff
painghtetaung <painghtetaung1999@gmail.com>
2021-08-15 16:57:43 +0630

    collisions,move and slide added

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,18 +2,32 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 
+const ACCELERATION = 500
+const MAX_SPEED = 800
+const FRICTION = 500
+
 func _physics_process(delta):
 #	the other method to move player	
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-
+	input_vector = input_vector.normalized()
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector	
+#		velocity += input_vector * ACCELERATION * dela
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+#		velocity = velocity.clamped(MAX_SPEED * delta)
+#		print(velocity)
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+#		print(velocity)	
+#	
+#	print(velocity)
+#	move_and_collide(velocity * delta)
 	
-	move_and_collide(velocity)	
+#	move_and_slide will handle the delta u dont need to add delta
+	velocity = move_and_slide(velocity) 
+	print(velocity)
+#	
 		
 #	
 #simple movement
