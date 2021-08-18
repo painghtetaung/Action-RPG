b534b0cc9655c3389fde38a99aabb8bc898bf5b2
painghtetaung <painghtetaung1999@gmail.com>
2021-08-18 14:00:09 +0630

    player animation idle and run added

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,8 +3,15 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 
 const ACCELERATION = 500
-const MAX_SPEED = 800
+const MAX_SPEED = 80
 const FRICTION = 500
+
+#var animationPlayer = null;
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+#func _ready():
+#	animationPlayer = $AnimationPlayer
 
 func _physics_process(delta):
 #	the other method to move player	
@@ -13,11 +20,15 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 #		velocity += input_vector * ACCELERATION * dela
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 #		velocity = velocity.clamped(MAX_SPEED * delta)
 #		print(velocity)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 #		print(velocity)	
 #	
